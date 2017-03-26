040d0091f6838057eab730238d5c1cde404f4f3e
Josh Ivars <josheivars@gmail.com>
2017-03-26 02:24:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  let node = document.getElementById("grand-node");
+  let nextNode = node.children[0];
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0];
+  }
+  return node;
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll(".ranked-list")
+  for (let i = 0, l = list.length; i < l; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
